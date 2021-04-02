b13e3003546e8274d2bc450bd8772e75d44af712
jakesig <jsigman04@gmail.com>
2021-04-02 16:41:58 -0400

    File Read 1

--- ProgrammingAssignment1/main.cpp
+++ ProgrammingAssignment1/main.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
 using namespace std;
 
 template <typename T> class SimpleList {
-    private:
-        int length;
-        Node *first;
-        Node *last;
-        class Node {
-            private:
-                T value;
-                T *next;
 
-            public:
-                T getValue() const {
-                    return value;
-                }
-                void setValue(T value) {
-                    Node::value = value;
-                }
-                T *getNext() const {
-                    return next;
-                }
-                void setNext(T *next) {
-                    Node::next = next;
-                }
-                Node(T value) : value(value) {
+    class Node {
+        private:
+            T value;
+            T *next;
 
-                }
+        public:
+            T getValue() const {
+                return value;
+            }
+            void setValue(T value) {
+                Node::value = value;
+            }
+            T *getNext() const {
+                return next;
+            }
+            void setNext(T *next) {
+                Node::next = next;
+            }
+            Node(T value) : value(value) {
+
+            }
         };
 
-    public:
         virtual void push(T item);
         virtual T pop();
         virtual T peek();
         SimpleList() {
 
         };
-}
+    private:
+        int length;
+        Node *first;
+        Node *last;
+};
 
 template <typename T> class Stack: public SimpleList<T> {
 
@@ -48,11 +51,18 @@ template <typename T> class Queue: public SimpleList<T> {
 
 };
 
-void readFile(char[] filename) {
-
-}
+void readFile(string fileName) {
+    ifstream file;
+    string out;
+    file.open(fileName);
+    while (getline(file, out)) {
+        cout << out;
+    }
+    file.close();
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    readFile("C:\\cygwin64\\home\\jsigm\\dsa2021spring\\ProgrammingAssignment1\\commands1.txt");
     return 0;
 }
