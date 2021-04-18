0a44fd351056b3dc32daec774116920e07d23cf2
jakesig <jsigman04@gmail.com>
2021-04-17 21:55:38 -0400

    Comments part 2, some implementation changes.

--- ProgrammingAssignment1/main.cpp
+++ ProgrammingAssignment1/main.cpp
@@ -107,6 +107,7 @@ template <typename T> class SimpleList {
 
             /** Base case: If the list is empty, set the first and
              *  last pointer equivalent to the node inputted.
+             *  Additionally increment the length.
              */
 
             if (isEmpty()) {
@@ -118,7 +119,7 @@ template <typename T> class SimpleList {
             /** Perform the insertion of the first node at the start
              *  by making a new node with the next pointer pointing to
              *  the first node, and the first pointer pointing to the
-             *  new node.
+             *  new node. Additionally increment the length.
              */
 
             auto insertion = new Node(newNode, first);
@@ -135,6 +136,7 @@ template <typename T> class SimpleList {
 
             /** Base case: If the list is empty, set the first and
              *  last pointer equivalent to the node inputted.
+             *  Additionally increment the length.
              */
 
             if (isEmpty()) {
@@ -146,7 +148,7 @@ template <typename T> class SimpleList {
             /** Perform the insertion of the last node at the end
              *  by reassigning the existing last node's next pointer
              *  to a new node and replacing the existing last pointer
-             *  with the new node.
+             *  with the new node. Additionally increment the length.
              */
 
             auto insertion = new Node(newNode);
@@ -183,13 +185,20 @@ template <typename T> class SimpleList {
                 first = last = nullptr;
             }
 
-
+            /** Case 3: If the SimpleList has more than one item, set the first
+             *  pointer to its next pointer.
+             */
 
             else {
                 first = first->next;
             }
-            length--;
+
+            /** Deallocate the memory for the node removed, decrement the length
+             *  and return the value stored in the removed node.
+             */
+
             delete(extraction);
+            length--;
             return returnVal;
         }
 
@@ -200,6 +209,11 @@ template <typename T> class SimpleList {
         virtual T pop() = 0;
 
 };
+
+/** Stack class used in the implementation.
+ *
+ *  @tparam T
+ */
 
 template <typename T> class Stack: public SimpleList<T> {
 
@@ -226,6 +240,11 @@ template <typename T> class Stack: public SimpleList<T> {
         }
 
 };
+
+/** Queue class used in the implementation.
+ *
+ *  @tparam T
+ */
 
 template <typename T> class Queue: public SimpleList<T> {
 
@@ -281,7 +300,7 @@ void split(string str, string output[]) {
 
 /** checkName(): Checks each map to see if there is a key containing a name of a SimpleList.
  *
- *  @param {string} name  The name of the SimpleList to look for.
+ *  @param {string} name The name of the SimpleList to look for.
  *  @param {map<string, SimpleList<int> *>} intMap The map containing integers to search.
  *  @param {map<string, SimpleList<double> *>} doubleMap The map containing doubles to search.
  *  @param {map<string, SimpleList<string> *>} strMap The map containing strings to search.
@@ -293,34 +312,69 @@ bool checkName(string key, map<string, SimpleList<int> *> intMap, map<string, Si
 
 /** readFile(): Reads from input file and processes the commands inside of it.
  *
- *  @param {string} fileName The name of the file to be read from.
+ *  @param {ifstream} inFile The input file that is being read from.
  *  @param {ofstream} outFile The output that is being written to.
  */
 
-void readFile(string fileName, ofstream& outFile) {
-    ifstream file;
+void readFile(ifstream& inFile, ofstream& outFile) {
+
+    /** Variable initializations for the function.
+     *  read - A string that will contain each line read from the input file.
+     *  processed[3] - An array of strings containing each component of read.
+     *  component1 - A string containing the first component of read.
+     *  name - A string containing the second component of read (or the name of the Stack/Queue).
+     *  component3 - A string containing the third component of read (if there is one).
+     *  type - A character containing the type of each Stack/Queue (or the first character of the name).
+     *  intMap - A map containing the name of each integer Stack/Queue and the Stack/Queue itself.
+     *  doubleMap - A map containing the name of each double Stack/Queue and the Stack/Queue itself.
+     *  stringMap - A map containing the name of each string Stack/Queue and the Stack/Queue itself.
+     */
+
     string read;
+    string processed[3];
     string component1;
     string name;
     string component3;
-    string processed[3];
     char type;
     map<string, SimpleList<int> *> intMap;
     map<string, SimpleList<double> *> doubleMap;
     map<string, SimpleList<string> *> strMap;
-    file.open(fileName);
-    while (getline(file, read)) {
+
+    /** Main loop for reading each line of the input file.
+     */
+
+    while (getline(inFile, read)) {
+
         outFile <<"PROCESSING COMMAND: " << read << "\n";
+
+        /** Divide each component of the command using the split() function
+         *  defined above, giving the corresponding variables their values.
+         */
+
         split(read, processed);
         component1 = processed[0];
         name = processed[1];
         type = name.at(0);
         component3 = processed[2];
+
+        /** First condition: If the value stored in component1 is "create",
+         *  create a new Stack/Queue based on what is in component3.
+         */
+
         if (component1 == "create") {
+
+            /** Check if the name stored already exists and if it does, output
+             *  to the file, and move along to process the next line.
+             */
+
             if (checkName(name, intMap, doubleMap, strMap)) {
                 outFile << "ERROR: This name already exists!" << "\n";
                 continue;
             }
+
+            /** If the value stored in component3 is "stack", create a new stack based on its type,
+             *  using a switch-case statement.
+             */
 
             if (component3 == "stack") {
                 switch (type) {
@@ -338,17 +392,35 @@ void readFile(string fileName, ofstream& outFile) {
                 }
             }
 
+            /** If the value stored in component3 is "queue", create a new queue based on its type,
+             *  using a switch-case statement.
+             */
+
             if (component3 == "queue") {
-                if (name.substr(0,1)=="i")
-                    intMap.insert(pair<string, SimpleList<int> *>(name, new Queue<int>()));
-                else if (name.substr(0,1)=="d")
-                    doubleMap.insert(pair<string, SimpleList<double> *>(name, new Queue<double>()));
-                else if (name.substr(0,1)=="s")
-                    strMap.insert(pair<string, SimpleList<string> *>(name, new Queue<string>()));
+                switch (type) {
+                    case 'i':
+                        intMap.insert(pair<string, SimpleList<int> *>(name, new Queue<int>()));
+                    case 'd':
+                        doubleMap.insert(pair<string, SimpleList<double> *>(name, new Queue<double>()));
+                    case 's':
+                        strMap.insert(pair<string, SimpleList<string> *>(name, new Queue<string>()));
+                    default:
+                        break;
+                }
             }
         }
 
+        /** Second condition: If the value stored in component1 is "push",
+         *  push the value of respective type in component3 onto the Stack/Queue
+         *  using a switch-case statement.
+         */
+
         if (component1 == "push") {
+
+            /** Check if the name stored already exists and if it does not, output
+             *  to the file, and move along to process the next line.
+             */
+
             if (!checkName(name, intMap, doubleMap, strMap)) {
                 outFile << "ERROR: This name does not exist!" << "\n";
                 continue;
@@ -368,7 +440,19 @@ void readFile(string fileName, ofstream& outFile) {
                     break;
             }
         }
+
+        /** Third condition: If the value stored in component1 is "pop",
+         *  pop the value of the Stack/Queue specified using a switch-case statement.
+         *  At the beginning of each case, check if the Stack/Queue is empty, and
+         *  if it is, output to the file, and move along to process the next line.
+         */
+
         if (component1 == "pop") {
+
+            /** Check if the name stored already exists and if it does not, output
+             *  to the file, and move along to process the next line.
+             */
+
             if (!checkName(name, intMap, doubleMap, strMap)) {
                 outFile << "ERROR: This name does not exist!" << "\n";
                 continue;
@@ -401,15 +485,40 @@ void readFile(string fileName, ofstream& outFile) {
             }
         }
     }
-    file.close();
+
+    /** Close both the output and input file.
+     */
+
+    inFile.close();
     outFile.close();
 };
 
+/** main() method.
+ *
+ * @return 0
+ */
+
 int main() {
-    ofstream file;
+
+    /** Create and open both an output file and input file,
+     *  prompting the user for names of both using getFileName().
+     */
+
+    ofstream outFile;
+    ifstream inFile;
     cout << "Output";
-    file.open(getFileName());
+    outFile.open(getFileName());
     cout << "Input";
-    readFile(getFileName(), file);
+    inFile.open(getFileName());
+
+    /** Process the commands of the input file, outputting to the output
+     *  file using the readFile() function.
+     */
+
+    readFile(inFile, outFile);
+
+    /** Return 0.
+     */
+
     return 0;
 }
